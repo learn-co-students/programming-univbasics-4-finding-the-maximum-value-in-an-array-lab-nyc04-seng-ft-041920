c8057a6e536334ec077e2f4367e91929bb5cf1ba
active-modem-5460 <jeffhv95@gmail.com>
2020-04-26 17:35:05 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,18 @@
 def find_max_value(array)
   # Add your solution here
+  max_num = 0
+  index = 0
+  while index < array.length do
+    current_num = array[index]
+    if current_num >= max_num
+      max_num = current_num
+    end
+    index += 1
+  end
+  max_num
 end
+#
+# def find_max_value(array)
+#   # Add your solution here
+#   array.max
+# end

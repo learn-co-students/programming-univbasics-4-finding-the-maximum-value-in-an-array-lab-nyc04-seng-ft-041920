c462d99e0a2820e198097c7a43b829494104a365
Hyojin Yoo <yoohyojin@gmail.com>
2020-04-29 03:15:07 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,15 @@
+
+#assume all arrays are sets of positive integers
+
 def find_max_value(array)
-  # Add your solution here
+  #array.sort.last
+  count = 0
+  max = -10
+  while count < array.length do
+    if max < array[count]
+      max = array[count]
+    end
+    count += 1
+  end
+  max
 end
